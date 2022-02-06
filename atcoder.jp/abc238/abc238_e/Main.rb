b3786b303877689fcbc35521787e9e3415c93e9c
m_asa_o <m38.ohashi@gmail.com>
2022-02-06 20:48:55 +0900

    [AC] abc238 abc238_e

---
+++ atcoder.jp/abc238/abc238_e/Main.rb
@@ -0,0 +1,34 @@
+class UnionFind
+  def initialize(size)
+    @rank = Array.new(size, 0)
+    @parent = Array.new(size, &:itself)
+  end
+
+  def unite(id_x, id_y)
+    x_parent = get_parent(id_x)
+    y_parent = get_parent(id_y)
+    return if x_parent == y_parent
+
+    if @rank[x_parent] > @rank[y_parent]
+      @parent[y_parent] = x_parent
+    else
+      @parent[x_parent] = y_parent
+      @rank[y_parent] += 1 if @rank[x_parent] == @rank[y_parent]
+    end
+  end
+
+  def get_parent(id_x)
+    @parent[id_x] == id_x ? id_x : (@parent[id_x] = get_parent(@parent[id_x]))
+  end
+
+  def same_parent?(id_x, id_y)
+    get_parent(id_x) == get_parent(id_y)
+  end
+end
+
+(n,q),*lr=$<.map{_1.split.map &:to_i}
+uf=UnionFind.new(n+1)
+lr.each{|l,r|
+  uf.unite(l-1,r)
+}
+puts uf.same_parent?(0,n) ? :Yes : :No
