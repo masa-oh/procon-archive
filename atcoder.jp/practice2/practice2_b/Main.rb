db71dd21b58496f77923006693430aabf884a689
m_asa_o <m38.ohashi@gmail.com>
2022-04-09 01:23:36 +0900

    [AC] practice2 practice2_b

---
+++ atcoder.jp/practice2/practice2_b/Main.rb
@@ -0,0 +1,75 @@
+# Fenwick Tree
+class FenwickTree
+  attr_reader :data, :size
+
+  def initialize(arg)
+    case arg
+    when Array
+      @size = arg.size
+      @data = [0].concat(arg)
+      (1 ... @size).each do |i|
+        up = i + (i & -i)
+        next if up > @size
+
+        @data[up] += @data[i]
+      end
+    when Integer
+      @size = arg
+      @data = Array.new(@size + 1, 0)
+    else
+      raise ArgumentError.new("wrong argument. type is Array or Integer")
+    end
+  end
+
+  def add(i, x)
+    i += 1
+    while i <= @size
+      @data[i] += x
+      i += (i & -i)
+    end
+  end
+
+  # .sum(l, r)  # [l, r)  <- Original
+  # .sum(r)     # [0, r)  <- [Experimental]
+  # .sum(l..r)  # [l, r]  <- [Experimental]
+  def sum(a, b = nil)
+    if b
+      _sum(b) - _sum(a)
+    elsif a.is_a?(Range)
+      l = a.begin
+      l += @size if l < 0
+      if r = a.end
+        r += @size if r < 0
+        r += 1 unless a.exclude_end?
+      else
+        r = @size
+      end
+      _sum(r) - _sum(l)
+    else
+      _sum(a)
+    end
+  end
+
+  def _sum(i)
+    res = 0
+    while i > 0
+      res += @data[i]
+      i &= i - 1
+    end
+    res
+  end
+  alias left_sum _sum
+end
+
+n,q=gets.split.map &:to_i
+fw=FenwickTree.new(gets.split.map &:to_i)
+ans=[]
+q.times do
+  a,b,c=gets.split.map &:to_i
+  if a==0
+    fw.add(b,c)
+  else
+    ans << fw.sum(b, c)
+  end
+end
+puts ans
