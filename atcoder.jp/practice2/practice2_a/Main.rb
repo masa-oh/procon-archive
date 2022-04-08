b4f3ae9595be73347c6f6e097a389063796a1375
m_asa_o <m38.ohashi@gmail.com>
2022-04-09 01:08:13 +0900

    [AC] practice2 practice2_a

---
+++ atcoder.jp/practice2/practice2_a/Main.rb
@@ -0,0 +1,60 @@
+# Disjoint Set Union
+class DSU
+  def initialize(n)
+    @n = n
+    @parent_or_size = Array.new(n, -1)
+    # root node: -1 * component size
+    # otherwise: parent
+  end
+
+  attr_reader :parent_or_size, :n
+
+  def merge(a, b)
+    x = leader(a)
+    y = leader(b)
+    return x if x == y
+
+    x, y = y, x if -@parent_or_size[x] < -@parent_or_size[y]
+    @parent_or_size[x] += @parent_or_size[y]
+    @parent_or_size[y] = x
+  end
+  alias unite merge
+
+  def same?(a, b)
+    leader(a) == leader(b)
+  end
+  alias same same?
+
+  def leader(a)
+    unless 0 <= a && a < @n
+      raise ArgumentError.new, "#{a} is out of range (0...#{@n})"
+    end
+
+    @parent_or_size[a] < 0 ? a : (@parent_or_size[a] = leader(@parent_or_size[a]))
+  end
+  alias root leader
+  alias find leader
+
+  def size(a)
+    -@parent_or_size[leader(a)]
+  end
+
+  def groups
+    (0 ... @parent_or_size.size).group_by{ |i| leader(i) }.values
+  end
+end
+
+UnionFind = DSU
+
+n,q=gets.split.map &:to_i
+i=0
+uf=UnionFind.new n
+while i<q
+  t,u,v=gets.split.map &:to_i
+  if t==0
+    uf.merge(u,v)
+  else
+    puts uf.same?(u,v) ? 1 : 0
+  end
+  i+=1
+end
