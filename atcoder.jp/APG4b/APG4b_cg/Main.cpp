753b1a0328a545d8ec27b4e471601b1daf10da5a
m_asa_o <m38.ohashi@gmail.com>
2022-04-16 11:45:56 +0900

    [AC] APG4b APG4b_cg

---
+++ atcoder.jp/APG4b/APG4b_cg/Main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  vector<int> data(5);
+  for (int i = 0; i < 5; i++) {
+    cin >> data.at(i);
+  }
+
+  // dataの中で隣り合う等しい要素が存在するなら"YES"を出力し、そうでなければ"NO"を出力する
+  bool flag = false;
+  for (int i=0; i < data.size(); i++) {
+    if (i==0) continue;
+
+    if (data.at(i-1) == data.at(i)) {
+      flag = true;
+      break;
+    }
+  }
+  
+  if (flag) {
+    cout << "YES" << endl;
+  }
+  else {
+    cout << "NO" << endl;
+  }
+}
